71247b4bbb805ae4dffd598827a267cb94831783
Marcus Barcelos <mvlbarcelos@gmail.com>
2013-05-10 19:22:01 -0300

    Criaçao e retorno de comentarios

--- src/main/java/com/mvlbarcelos/controller/CommentController.java
+++ src/main/java/com/mvlbarcelos/controller/CommentController.java
@@ -1,15 +1,20 @@
 package com.mvlbarcelos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mvlbarcelos.dao.CommentDAO;
 import com.mvlbarcelos.entity.Comment;
+
 
 @Controller
 public class CommentController {
+
+	private CommentDAO commentDAO;
 
 	@RequestMapping("/")
 	public ModelAndView index() {
@@ -17,7 +22,7 @@ public class CommentController {
 
 		return view;
 	}
-	
+
 	@RequestMapping(value = "/{titleUrl}") 
 	public ModelAndView createComment(@PathVariable("titleUrl") String titleUrl, @RequestParam(required = false) String title) {
 		Comment comment = new Comment();
@@ -28,5 +33,25 @@ public class CommentController {
 		modelAndView.addObject("comment", comment);
 
 		return modelAndView;
+	}
+
+	@RequestMapping(value = "salvar/{titleUrl}")
+	public ModelAndView save (@PathVariable("titleUrl") String titleUrl, String email, String comment, String title) {
+		Comment c = new Comment();
+		c.setTitleUrl(titleUrl);
+		c.setEmail(email);
+		c.setComment(comment);
+		c.setTitle(title);
+		commentDAO.save(c);
+
+		ModelAndView modelAndView = new ModelAndView("list");
+		modelAndView.addObject("comments", commentDAO.listCommentsByTitleUr(titleUrl));
+
+		return modelAndView;
+	}
+
+	@Autowired
+	public void setCommentDAO(CommentDAO commentDAO) {
+		this.commentDAO = commentDAO;
 	}
 }
--- src/test/java/com/mvlbarcelos/controller/CommentControllerTest.java
+++ src/test/java/com/mvlbarcelos/controller/CommentControllerTest.java
@@ -3,19 +3,32 @@ package com.mvlbarcelos.controller;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import static org.mockito.Mockito.*;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mvlbarcelos.dao.CommentDAO;
 import com.mvlbarcelos.entity.Comment;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CommentControllerTest {
-	
+
 	private CommentController commentController;
-	
+
+	@Mock
+	private CommentDAO commentDAO;
+
 	@Before
 	public void setup() throws Exception {
 		commentController = new CommentController();
+		commentController.setCommentDAO(commentDAO);
 	}
 	
 	@Test
@@ -33,5 +46,27 @@ public class CommentControllerTest {
 		assertThat(modelAndView.getViewName(), is("detail"));
 		assertThat(comment.getTitleUrl(), is("metodo-ageis"));
 		assertThat(comment.getTitle(), is("metodo çgeis"));
+	}
+
+	@Test
+	public void shouldSaveCommentAndReturnComments() throws Exception {
+		String titleUrl = "metodo-ageis";
+		List<Comment> comments = getComments();
+		when(commentDAO.listCommentsByTitleUr(titleUrl)).thenReturn(comments );
+
+		ModelAndView modelAndView = commentController.save(titleUrl, "teste@teste,com", "metodos ageis", "metodos ‡geis");
+
+		assertThat(modelAndView.getViewName(), is("list"));
+		assertThat(comments, is(modelAndView.getModelMap().get("comments")));
+		verify(commentDAO).save(any(Comment.class));
+		verify(commentDAO).listCommentsByTitleUr(titleUrl);
+	}
+
+	private List<Comment> getComments() {
+		List<Comment> comments = new ArrayList<Comment>();
+		Comment comment = new Comment();
+		comment.setId(999L);
+
+		return comments;
 	}
 }
